83e7a46b6bf6a8284c421bb43b55f369ff48a9b5
jiemei-520 <1428429526@qq.com>
2020-12-03 15:19:41 +0800

    vue商城项目

---
+++ src/components/BgBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="bg-box">
+      <!-- <div class="bg"></div> -->
+      <div class="bg-content">
+        <slot></slot>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'BgBox'
+  }
+</script>
+
+<style lang="less" scoped>
+  .bg-box{
+
+    .bg{
+      height: 100px;
+      background-color: #ccc;
+    }
+
+    // .bg-content{
+    //   margin: 0 10px;
+    //   position: relative;
+    //   top: 100px;
+    //   // background-color: #000;
+    //   border-radius: 8px;
+    //   padding: 10px;
+    //   overflow: hidden;
+    // }
+    .bg-content{
+      margin: 0 10px;
+      position: relative;
+      top: 100px;
+      background-color:  #3a3b8b;
+      border-radius: 8px;
+      padding: 10px;
+      overflow: hidden;
+    }
+  }
+</style>
